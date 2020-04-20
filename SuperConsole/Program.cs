735cbd409f8aa67afa988d0cad5619f3627dfa79
Admin501 <Admin501@DESKTOP-HHG2HUE>
2020-04-21 02:11:19 +0300

    Add project files.

---
+++ SuperConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SuperConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*********************************");
+            Console.WriteLine("****Welcome to My Rocking App****");
+            Console.WriteLine("*********************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.ReadLine();
+
+            MessageBox.Show("Готово!"); 
+
+
+        }
+    }
+}
---
+++ SuperConsole/SuperCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperConsole
+{
+    public class SuperCar : Car
+    {
+        public string GetPetName()
+        {
+            PetName = "Fred";
+            return PetName;
+        }
+    }
+}
